bb1864f71f0bac18375862dea6ca8724d12aca4b
muneerahp <m.a.patel@sheffield.ac.uk>
2023-01-27 14:37:08 +0000

    setting pgpassword to be the default value looked for by postgres

--- docker/docker_setup.py
+++ docker/docker_setup.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
         # Database configuration
         config_parser['DATABASE']['db_name'] = os.environ['POSTGRES_DB']
         config_parser['DATABASE']['db_user'] = os.environ['POSTGRES_USER']
-        config_parser['DATABASE']['db_password'] = os.environ['POSTGRES_PASSWORD']
+        config_parser['DATABASE']['db_password'] = os.environ['PGPASSWORD']
 
         # Ensure Flask knows public port
         config_parser.add_section('SERVER')
@@ -122,7 +122,7 @@ if __name__ == "__main__":
 
         config_parser['DATABASE']['db_name'] = os.environ['POSTGRES_DB']
         config_parser['DATABASE']['db_user'] = os.environ['POSTGRES_USER']
-        config_parser['DATABASE']['db_password'] = os.environ['POSTGRES_PASSWORD']
+        config_parser['DATABASE']['db_password'] = os.environ['PGPASSWORD']
 
         public_port = os.environ['PUBLIC_PORT']
         config_parser['SERVER']['public_port'] = str(public_port)
